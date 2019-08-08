afcdc1b1ad44780bd54fd89c6a64253c65c14f4b
EnriqueSau <ruben.saucedo@wizeline.com>
2019-08-08 14:26:40 -0500

    Basic config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node-modules/
+            },
+            {
+              test: /\.(eot|woff|ttf|svg|png|jpg)$/,
+              loader: "file-loader"
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, '/public')
+    }
+}
