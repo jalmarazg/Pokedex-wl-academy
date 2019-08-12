a2ec39504a49b1d63a13ebca4b0f81e192eef695
jalmarazg <jalmarazgon@gmail.com>
2019-08-11 22:12:43 -0500

    Add changes for home index

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { isEmpty } from 'lodash'
+import axios from 'axios'
+import { REACT_POKEMON_ENDPOINT_API } from '../../utils/constants'
+import PokemonCard from '../../components/PokemonCard'
 
-const Home = () => {
-  return (
-    <div>
-      <p>
-          Home de Aplicacion de Pokedex
-      </p>
-    </div>
-  )
+class Home extends Component {
+  state = { pokemonData: {} }
+
+  componentDidMount () {
+    axios
+      .get(REACT_POKEMON_ENDPOINT_API)
+      .then((res) => {
+        this.setState({ pokemonData: res.data.pokemon })
+      })
+  }
+
+  render () {
+    const { pokemonData } = this.state
+    return (
+      <div>
+        <p>Home de Aplicacion de Pokedex</p>
+        { !isEmpty(pokemonData)
+          ? <PokemonCard
+            englishName={pokemonData.name[0].english}
+            idNumber={pokemonData.id}
+            image={pokemonData.url_image}
+            japaneseName={pokemonData.name[0].japanese}
+            stats={pokemonData.base}
+            type={pokemonData.type}
+          />
+          : <p>
+            Cargando...
+          </p>
+        }
+      </div>
+    )
+  }
 }
 
 export default Home
